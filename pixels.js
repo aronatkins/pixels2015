66e5eccbf8b61abd2a2d9c455dfd3981c24d5e7d
Aron Atkins <aron@gweep.net>
2015-03-25 22:22:39 -0400

    A new version of penguins. Web Audio. ASCII brilliance.

---
+++ pixels.js
@@ -0,0 +1,110 @@
+function squaak() {
+  var space = 5;
+  var audio;
+  var osc;
+  try {
+    if ('AudioContext' in window) {
+      audio = new AudioContext();
+    } else if ('webkitAudioContext' in window) {
+      audio = new webkitAudioContext();
+    } else {
+      throw new Error('no audio context');
+    }
+
+    osc = audio.createOscillator();
+    osc.connect(audio.destination);
+
+  } catch (err) {
+    alert("Try another browser Firefox or Chrome for sound.");
+    console.log('audio error',err);
+  }
+
+  var width = Math.max.apply(null, tux.map( function(s) { return s.length; } ));
+  var remaining = tux.length * width;
+  var coords = [];
+
+  function coord_to_id(x,y) {
+    return "cell_"+x+"_"+y;
+  }
+
+  function pad_lines(line) {
+    return line + Array(width - line.length + 1).join(' ');
+  }
+
+  function line_to_row(line,i) {
+    function c_to_cell(c,j,line) {
+      var id = coord_to_id(j,i);
+
+      return '<td id="'+id+'">' + c + '</td>';
+    }
+
+    var cells = line.split('').map(c_to_cell);
+    return '<tr>' + cells.join('') + '</tr>'
+  }
+
+  function tux_to_table() {
+    var rows = tux.map(pad_lines).map(line_to_row);
+
+    return '<table>\n' + rows.join('\n') + '</table>';
+  }
+
+  function play(x,y,s) {
+    if (osc) {
+      if (s != ' ') {
+        var c = s.charCodeAt(0);
+        // http://en.wikipedia.org/wiki/Piano_key_frequencies
+        var key = (c % 88); // 88 keys.
+        var note = Math.pow(2, (key - 49) / 12) * 440;
+
+        // console.log('char:',s,'note:',note);
+        osc.frequency.value = note;
+      }
+    }
+  }
+
+  function colorize() {
+    function clr() {
+      var n = Math.round(255 * Math.random());
+      var c = n.toString(16);
+      if (c.length == 1) {
+        c = "0" + c;
+      }
+      return c;
+    }
+    return "#" + clr() + clr() + clr();
+  }
+
+  function toggle(x,y) {
+    var id = coord_to_id(x,y);
+    var e = document.getElementById(id);
+    play(x,y,e.innerHTML);
+    e.style.visibility = 'visible';
+    e.style.color = colorize();
+  }      
+
+  function appear() {
+    if (coords.length > 0) {
+      var i = Math.round((coords.length - 1) * Math.random());
+      var coord = coords[i];
+      coords.splice(i, 1);
+      toggle(coord.x,coord.y);
+      setTimeout(appear, space);
+    } else {
+      if (osc) { osc.stop(0); }
+      console.log('squaak!');
+    }
+  }
+
+  for (var i=0; i<tux.length; i++) {
+    for (var j=0; j<width; j++) {
+      coords.push({ x: j, y: i});
+    }
+  }
+
+  document.getElementById('squaak').innerHTML = tux_to_table();
+
+  if (osc) {
+    osc.start(0);
+  }
+  setTimeout(appear, space);
+}
