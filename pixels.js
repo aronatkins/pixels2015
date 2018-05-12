9f13a5af69823b2340bcb507a9fd4581ceade719
Aron Atkins <aron@gweep.net>
2018-05-12 11:40:50 -0400

    formatting with prettier-js

--- pixels.js
+++ pixels.js
@@ -3,57 +3,61 @@ function pixels(id, data) {
   var audio;
   var osc;
   try {
-    if ('AudioContext' in window) {
+    if ("AudioContext" in window) {
       audio = new AudioContext();
-    } else if ('webkitAudioContext' in window) {
+    } else if ("webkitAudioContext" in window) {
       audio = new webkitAudioContext();
     } else {
-      throw new Error('no audio context');
+      throw new Error("no audio context");
     }
 
     osc = audio.createOscillator();
     osc.connect(audio.destination);
-
   } catch (err) {
-    console.log('audio error',err);
+    console.log("audio error", err);
     return;
   }
 
-  var width = Math.max.apply(null, data.map( function(s) { return s.length; } ));
+  var width = Math.max.apply(
+    null,
+    data.map(function(s) {
+      return s.length;
+    })
+  );
   var remaining = data.length * width;
   var coords = [];
 
-  function coord_to_id(x,y) {
-    return "cell_"+x+"_"+y;
+  function coord_to_id(x, y) {
+    return "cell_" + x + "_" + y;
   }
 
   function pad_lines(line) {
-    return line + Array(width - line.length + 1).join(' ');
+    return line + Array(width - line.length + 1).join(" ");
   }
 
-  function line_to_row(line,i) {
-    function c_to_cell(c,j,line) {
-      var id = coord_to_id(j,i);
+  function line_to_row(line, i) {
+    function c_to_cell(c, j, line) {
+      var id = coord_to_id(j, i);
 
-      return '<td id="'+id+'">' + c + '</td>';
+      return '<td id="' + id + '">' + c + "</td>";
     }
 
-    var cells = line.split('').map(c_to_cell);
-    return '<tr>' + cells.join('') + '</tr>'
+    var cells = line.split("").map(c_to_cell);
+    return "<tr>" + cells.join("") + "</tr>";
   }
 
   function data_to_table() {
     var rows = data.map(pad_lines).map(line_to_row);
 
-    return '<table>\n' + rows.join('\n') + '</table>';
+    return "<table>\n" + rows.join("\n") + "</table>";
   }
 
-  function play(x,y,s) {
+  function play(x, y, s) {
     if (osc) {
-      if (s != ' ') {
+      if (s != " ") {
         var c = s.charCodeAt(0);
         // http://en.wikipedia.org/wiki/Piano_key_frequencies
-        var key = (c % 88); // 88 keys.
+        var key = c % 88; // 88 keys.
         var note = Math.pow(2, (key - 49) / 12) * 440;
 
         // console.log('char:',s,'note:',note);
@@ -74,30 +78,32 @@ function pixels(id, data) {
     return "#" + clr() + clr() + clr();
   }
 
-  function toggle(x,y) {
-    var id = coord_to_id(x,y);
+  function toggle(x, y) {
+    var id = coord_to_id(x, y);
     var e = document.getElementById(id);
-    play(x,y,e.innerHTML);
-    e.style.visibility = 'visible';
+    play(x, y, e.innerHTML);
+    e.style.visibility = "visible";
     e.style.color = colorize();
-  }      
+  }
 
   function appear() {
     if (coords.length > 0) {
       var i = Math.round((coords.length - 1) * Math.random());
       var coord = coords[i];
       coords.splice(i, 1);
-      toggle(coord.x,coord.y);
+      toggle(coord.x, coord.y);
       setTimeout(appear, space);
     } else {
-      if (osc) { osc.stop(0); }
-      console.log('done!');
+      if (osc) {
+        osc.stop(0);
+      }
+      console.log("done!");
     }
   }
 
-  for (var i=0; i<data.length; i++) {
-    for (var j=0; j<width; j++) {
-      coords.push({ x: j, y: i});
+  for (var i = 0; i < data.length; i++) {
+    for (var j = 0; j < width; j++) {
+      coords.push({ x: j, y: i });
     }
   }
 
